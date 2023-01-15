13a4a2ea9ecccc52401f019869a5e0f7ec3e1ca6
musa1992 <mosesdev92@gmail.com>
2023-01-15 13:31:09 +0300

    add styles for mobile view for CountryDetails component

--- src/components/country/CountryDetails.js
+++ src/components/country/CountryDetails.js
@@ -5,67 +5,73 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 const CountryDetails = props => {
   return (
-    <div>
-      <button><FontAwesomeIcon icon={faArrowLeft}/> Back</button>
-      <div className='flex flex-mobile-column'>
+    <div className='container details-page'>
+      <button className='btn'><FontAwesomeIcon icon={faArrowLeft} className='btn-icon'/> Back</button>
+      <div className='flex flex-mobile-column details-section'>
         <div className='country-details-flag'>
             <p>FLAG</p>
         </div>
         <div>
-            <p>Belgium</p>
-            <div className='country-details'>
+            <p className='xlg-text'>Belgium</p>
+            <div className='country-full-details'>
                 <div className='flex flex-mobile-column'>
                     <div className='geographic-details'>
                       <div className='flex'>
-                          <p>Native Name:</p>
+                          <p className='text-bold'>Native Name:</p>
                           <p>Belgie</p>
                       </div>
                       <div className='flex'>
-                          <p>Population:</p>
+                          <p className='text-bold'>Population:</p>
                           <p>11,319,511</p>
                       </div>
                       <div className='flex'>
-                          <p>Region:</p>
+                          <p className='text-bold'>Region:</p>
                           <p>Europe</p>
                       </div>
                       <div className='flex'>
-                          <p>Sub Region:</p>
+                          <p className='text-bold'>Sub Region:</p>
                           <p>Western Europe</p>
                       </div>
                       <div className='flex'>
-                          <p>Capital:</p>
+                          <p className='text-bold'>Capital:</p>
                           <p>Brussels</p>
                       </div>
                     </div>
                     <div className='demographic-details'>
                       <div className='flex'>
-                          <p>Top Level Domain:</p>
-                          <p>be</p>
+                          <p className='text-bold'>Top Level Domain:</p>
+                          <p>.be</p>
                       </div>
                       <div className='flex'>
-                          <p>Currencies:</p>
+                          <p className='text-bold'>Currencies:</p>
                           <p>Euro</p>
                       </div>
                       <div className='flex'>
-                          <p>Languages:</p>
+                          <p className='text-bold'>Languages:</p>
                           <div className='flex'>
                             <p>Dutch,</p>
-                            <p>French</p>
+                            <p>French,</p>
                             <p>German</p>
                           </div>
                       </div>
                     </div>
                 </div>
-                <div className='flex border-neighbours'>
+                <div className='flex flex-mobile-column border-neighbours'>
                     <div className='border-title'>
-                      <p>Border Neighbours:</p>
+                      <p className='text-bold text-lg'>Border Countries:</p>
                     </div>
-                    <div className='flex'>
+                    <div className='border-grid'>
                       <div className='neighbour'>
                         <p>France</p>
                       </div>
                       <div className='neighbour'>
                         <p>Germany</p>
+                      </div>
+                      <div className='neighbour'>
+                        <p>Netherlands</p>
+                      </div>
+                      <div className='neighbour'>
+                        <p>Netherlands</p>
                       </div>
                       <div className='neighbour'>
                         <p>Netherlands</p>
